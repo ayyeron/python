cd347e5f3535f0e8ccc128fc712ce2d4db295269
ayyeron <102568393+ayyeron@users.noreply.github.com>
2022-03-28 20:04:08 -0700

    Add files via upload

---
+++ MIni Project 1 python.py
@@ -0,0 +1,10 @@
+noun =input("noun: ")
+adj =input("adjective: ")
+plnoun =input("plural noun: ")
+partofbody =input("part of body: ")
+number =input("number: ")
+place =input("place: ")
+verbing =input("verb/ing: ")
+
+
+madlib =f"I want to go on vacation at {place}, I want to {verbing} at the most tropical beaches there. The local people are very {adj}, they introduced themselves using their tradtional greetings. The {noun} was very diffcult to managed, until i completely gave up. I ate {number} {plnoun} and it was incredible. Afterwards my {partofbody} was super sore and in extreme amount of pain. "
